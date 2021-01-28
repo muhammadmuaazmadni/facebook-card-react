1c638368473ec5f9e34c953dd3a60528cd979609
muhammadmuaazmadni <muhammadmuaazmadni@gmail.com>
2021-01-28 17:39:04 +0500

    react card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
+
+function Hi (props) {
+  return <div className="name">
+    {/* <strong>Hello{props.mame}</strong>
+    <div className="center">
+<h3>Some Info</h3>
+<ul>
+<li>Name :{props.name}</li>
+<li>F Name :{props.fname}</li>
+<li>Age :{props.age}</li>
+<li>Email :{props.email}</li>
+
+</ul>
+<ul>
+<h3>Some Math Operation</h3>
+  <li>Add :{props.add}{10+10}</li>
+  <li>Minus :{props.minus}{20-10}</li>
+  <li>multiply :{props.multiply}{3*5}</li>
+
+</ul>
+
+
+
+    </div> */}
+    <ul>
+      <li><h2> Title{props.title} </h2></li>
+      <li><p> Text {props.text}</p></li>
+      <li>Imageurl<img src={props.image} /></li>
+
+    </ul>
+    <ul>
+      <li><h2> Title{props.title} </h2></li>
+      <li><p> Text {props.text}</p></li>
+      <li>Imageurl<img src={props.image} /></li>
+
+    </ul>
+  </div>
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <>
+<Hi title="alone but happy" text="live happily" image="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg" name="muaaz" fname="abdul qadir" age="21" email="muhammadmuaazmadni@gmail.com" add="{10+10}" minus="{20-10}" multiply="{3*5}" />
+</>,
+ document.querySelector('#root'));
